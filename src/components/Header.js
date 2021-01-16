6e6679be3753e4e89bcc2d7a3b26653f66888b43
Robzlegz <robzlegz@gmail.com>
2021-01-16 17:41:06 +0200

    adding-fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import "./Header.css";
+import TeslaLogo from "../assets/teslaLogoSmall.svg";
 
 function Header() {
     return (
         <div className="header">
-            
+            <div className="header-logo">
+                <img src={TeslaLogo} alt="tesla logo"/>
+            </div>
+            <div className="header-center">
+                <p>Model S</p>
+                <p>Model 3</p>
+                <p>Model X</p>
+                <p>Model Y</p>
+                <p>Solar Roof</p>
+                <p>Solar Panel</p>
+            </div>
+            <div className="header-right">
+                <p>Shop</p>
+                <p>Tesla Account</p>
+            </div>
         </div>
     )
 }
