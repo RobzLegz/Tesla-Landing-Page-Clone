d8b4c40616449e918fff967ffc01c7bddb8b35c1
Robzlegz <robzlegz@gmail.com>
2021-01-17 16:24:48 +0200

    react router-stuff

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from "react";
 import './App.css';
 import Header from "./components/Header";
-import { Switch, Route, useLocation } from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 
 function App() {
-
   return (
-    <div className="App">
-      <Header />
-      <Switch location={useLocation} key={useLocation.pathname}>
-        <Route path="/" exact>
-          <HomePage />
-        </Route>
-      </Switch>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
