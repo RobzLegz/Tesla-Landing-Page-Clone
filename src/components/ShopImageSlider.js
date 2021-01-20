3437f5fb0181d2da8950220f331b9eed605ff628
Robzlegz <robzlegz@gmail.com>
2021-01-20 08:59:18 +0200

    trying to use useEffect

--- src/components/ShopImageSlider.js
+++ src/components/ShopImageSlider.js
@@ -9,57 +9,70 @@ import Xweathermats from "../assets/Desktop-xweathermats.jpeg";
 import Wheelkit from "../assets/Desktop-wheelkit.jpeg";
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import React, {useEffect} from "react";
 
 function ShopImageSlider(){
+    
+    loadAllElements()
 
-    const imageSlide = document.querySelector(".image-slide");
-    const imgSize = 612.96;
-    let imgCounter = 1;
-    imageSlide.style.transform = 'translateX(' + (-imgSize * imgCounter) + "px)";
-    const carouselFwards = () => {
-        imgCounter ++;
+    useEffect(() => {
+        const imageSlide = document.querySelector(".image-slide");
+        const btnBack = document.querySelector(".back-btn");
+        const btnFwards = document.querySelector(".fwards-btn");
+        const imgSize = 612.96;
+        let imgCounter = 1;
         imageSlide.style.transform = 'translateX(' + (-imgSize * imgCounter) + "px)";
-        imageSlide.style.transition = "transform 0.4s ease-in-out";
-        console.log(imgCounter);
-        if(imgCounter >= 8){
-            imgCounter = 0;
-            imageSlide.style.transition = "none";
+        const carouselFwards = () => {
+            imgCounter ++;
             imageSlide.style.transform = 'translateX(' + (-imgSize * imgCounter) + "px)";
-        }
-    }
-    const carouselBack = () => {
-        imgCounter --;
-        imageSlide.style.transform = 'translateX(' + (-imgSize * imgCounter) + "px)";
-        console.log(imgCounter);
-        if(imgCounter <= -1){
-            imgCounter = 6;
-            imageSlide.style.transition = "none";
-            imageSlide.style.transform = 'translateX(' + (-imgSize * imgCounter) + "px)";
-        }else{
             imageSlide.style.transition = "transform 0.4s ease-in-out";
+            console.log(imgCounter);
+            if(imgCounter >= 8){
+                imgCounter = 0;
+                imageSlide.style.transition = "none";
+                imageSlide.style.transform = 'translateX(' + (-imgSize * imgCounter) + "px)";
+            }
         }
-    }
+        btnFwards.addEventListener("click", function(){
+            carouselFwards();
+        })
+        btnBack.addEventListener("click",function(){
+            imgCounter --;
+            imageSlide.style.transform = 'translateX(' + (-imgSize * imgCounter) + "px)";
+            console.log(imgCounter);
+            if(imgCounter <= -1){
+                imgCounter = 6;
+                imageSlide.style.transition = "none";
+                imageSlide.style.transform = 'translateX(' + (-imgSize * imgCounter) + "px)";
+            }else{
+                imageSlide.style.transition = "transform 0.4s ease-in-out";
+            }
+        })
+            
+    }, [loadAllElements])
 
-    return (
-        <>
-            <div className="image-slider">
-                <button className="btn-slide back-btn" onClick={carouselBack}><ArrowBackIosIcon /></button>
-                <div className="image-slide">
-                    <img className="carousel-img" id="lastagain" src={Diecast} alt="shop product img"/>  
-                    <img className="carousel-img" src={Mobileconnector} alt="shop product img"/>
-                    <img className="carousel-img" src={Cargomats} alt="shop product img"/>
-                    <img className="carousel-img" src={Shirt} alt="shop product img"/>
-                    <img className="carousel-img" src={Xweathermats} alt="shop product img"/>
-                    <img className="carousel-img" src={Wheelkit} alt="shop product img"/>
-                    <img className="carousel-img" src={Weathermats} alt="shop product img"/>
-                    <img className="carousel-img" src={RepairKit} alt="shop product img"/>
-                    <img className="carousel-img" src={Diecast} alt="shop product img"/>        
-                    <img className="carousel-img" id="firstagain" src={Mobileconnector} alt="shop product img"/>                      
+    function loadAllElements(){
+        return (
+            <>
+                <div className="image-slider">
+                    <button className="btn-slide back-btn"><ArrowBackIosIcon /></button>
+                    <div className="image-slide">
+                        <img className="carousel-img" id="lastagain" src={Diecast} alt="shop product img"/>  
+                        <img className="carousel-img" src={Mobileconnector} alt="shop product img"/>
+                        <img className="carousel-img" src={Cargomats} alt="shop product img"/>
+                        <img className="carousel-img" src={Shirt} alt="shop product img"/>
+                        <img className="carousel-img" src={Xweathermats} alt="shop product img"/>
+                        <img className="carousel-img" src={Wheelkit} alt="shop product img"/>
+                        <img className="carousel-img" src={Weathermats} alt="shop product img"/>
+                        <img className="carousel-img" src={RepairKit} alt="shop product img"/>
+                        <img className="carousel-img" src={Diecast} alt="shop product img"/>        
+                        <img className="carousel-img" id="firstagain" src={Mobileconnector} alt="shop product img"/>                      
+                    </div>
+                    <button className="btn-slide fwards-btn"><ArrowForwardIosIcon /></button>
                 </div>
-                <button className="btn-slide fwards-btn" onClick={carouselFwards}><ArrowForwardIosIcon /></button>
-            </div>
-        </>
-    )
+            </>
+        )
+    }
 }
 
 export default ShopImageSlider;
